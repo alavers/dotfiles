61ef4b69e1eca8c729a0af736b0cff61593a5235
Andrew Lavers <alavers@zendesk.com>
2022-09-08 12:35:33 -0400

    Add better zprofile, fix symlink install

--- install/link.sh
+++ install/link.sh
@@ -2,10 +2,13 @@
 
 DOTFILES=$HOME/.dotfiles
 
+get_linkables() {
+    find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
+}
+
 echo -e "\nCreating symlinks"
 echo "=============================="
-linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
-for file in $linkables ; do
+for file in $(get_linkables); do
     target="$HOME/.$( basename $file ".symlink" )"
     if [ -e $target ]; then
         echo "~${target#$HOME} already exists... Skipping."
