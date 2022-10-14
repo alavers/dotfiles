02f67ef8b97051d8a6b29d55dd5f92851dd8da07
Andrew Lavers <alavers@zendesk.com>
2022-10-14 09:31:55 -0400

    Change snippet tab to C-L/C-H

--- config/nvim/after/plugin/luasnip.rc.lua
+++ config/nvim/after/plugin/luasnip.rc.lua
@@ -1,7 +1,7 @@
-vim.keymap.set('s', '<Tab>', '<cmd>lua require"luasnip".jump(1)<cr>', { silent = true })
-vim.keymap.set('s', '<S-Tab>', '<cmd>lua require"luasnip".jump(-1)<cr>', { silent = true })
-vim.keymap.set('i', '<Tab>', '<cmd>lua require"luasnip".jump(1)<cr>', { silent = true })
-vim.keymap.set('i', '<S-Tab>', '<cmd>lua require"luasnip".jump(-1)<cr>', { silent = true })
+vim.keymap.set('s', '<C-L>', '<cmd>lua require"luasnip".jump(1)<cr>', { silent = true })
+vim.keymap.set('s', '<C-H>', '<cmd>lua require"luasnip".jump(-1)<cr>', { silent = true })
+vim.keymap.set('i', '<C-L>', '<cmd>lua require"luasnip".jump(1)<cr>', { silent = true })
+vim.keymap.set('i', '<C-H>', '<cmd>lua require"luasnip".jump(-1)<cr>', { silent = true })
 
 -- https://github.com/rafamadriz/friendly-snippets
 require('luasnip.loaders.from_vscode').lazy_load({ paths = { '~/.config/nvim/snippets' } })
