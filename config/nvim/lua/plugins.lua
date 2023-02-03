8a0956167d0bf9479bca4926b732ac0b99697e87
Andrew Lavers <alavers@zendesk.com>
2023-02-02 21:32:45 -0500

    Clever vim auto jumplist with no args

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -9,6 +9,7 @@ vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim' -- Common utilities
 
   use 'mileszs/ack.vim' -- File search
   use 'ellisonleao/gruvbox.nvim'
@@ -54,7 +55,6 @@ packer.startup(function(use)
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
 
-  use 'nvim-lua/plenary.nvim' -- Common utilities
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
   use 'ThePrimeagen/harpoon'
