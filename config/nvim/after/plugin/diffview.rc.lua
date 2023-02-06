f38ddaad64125d5072dd0e031414bf45210fc08e
Andrew Lavers <alavers@zendesk.com>
2023-02-06 12:02:03 -0500

    Tidy up diffview keymaps

--- config/nvim/after/plugin/diffview.rc.lua
+++ config/nvim/after/plugin/diffview.rc.lua
@@ -1,5 +1,8 @@
 local status, diffview = pcall(require, 'diffview')
 if (not status) then return end
 
-vim.keymap.set('n', '<leader>gd', '<Cmd>DiffviewOpen<CR>', {})
-vim.keymap.set('n', '<leader>gc', '<Cmd>DiffviewClose<CR>', {})
+vim.keymap.set('n', '<leader>do', '<Cmd>DiffviewOpen<CR>', {})
+vim.keymap.set('n', '<leader>dc', '<Cmd>DiffviewClose<CR>', {})
+vim.keymap.set('n', '<leader>o', function()
+  require('diffview.config').actions.focus_entry()
+end)
