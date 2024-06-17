9ecc784538834b800f5c0da33e9669fed1a9fa60
Andrew Lavers <alavers@zendesk.com>
2024-06-17 13:03:24 -0400

    Reactivate lspconfig and lspsaga hotkeys

--- config/nvim/plugin/lspsaga.rc.lua
+++ config/nvim/plugin/lspsaga.rc.lua
@@ -33,7 +33,7 @@ vim.keymap.set('n', ']h', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', '[h', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
 
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga finder<CR>', opts)
+vim.keymap.set('n', 'gh', '<Cmd>Lspsaga finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
 vim.keymap.set('n', '<leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
