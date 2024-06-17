8a82635d0d07133b6ac9f01bf01f48d2a0edce5d
Andrew Lavers <alavers@zendesk.com>
2024-06-17 13:40:33 -0400

    Add search shortcut that excludes test dir

--- config/nvim/plugin/ack.rc.lua
+++ config/nvim/plugin/ack.rc.lua
@@ -1,1 +1,9 @@
 vim.g['ackprg'] = 'ag --path-to-ignore ~/.ignore --vimgrep'
+
+vim.api.nvim_create_user_command(
+  'Ac',
+  function(opts)
+    vim.cmd('Ack --ignore-dir=src/test ' .. opts.args)
+  end,
+  { nargs = 1 }
+)
