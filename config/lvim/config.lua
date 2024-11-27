2c060301e30333c6ec7b2f9c6a035d157a163b21
Andrew Lavers <alavers@zendesk.com>
2024-11-27 10:09:20 -0500

    Add prettier

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -110,6 +110,7 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "goimports", filetypes = { "go" } },
   { command = "gofumpt",   filetypes = { "go" } },
+  { name = "prettier", filetypes = { "javascript", "typescript", "typescriptreact" } }
 }
 
 lvim.format_on_save = {
