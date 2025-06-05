fabee50e817552dd4a5d710f4e4e96971c6457e7
Andrew Lavers <alavers@zendesk.com>
2025-06-05 17:51:51 -0400

    Update dotfiles

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -32,6 +32,7 @@ lvim.plugins = {
     cmd = { "G", "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse", "GRemove", "GRename", "Glgrep", "Gedit" },
     ft = { "fugitive" }
   },
+  'tpope/vim-rhubarb',
   'benfowler/telescope-luasnip.nvim',
 
   -- Theme
@@ -40,11 +41,93 @@ lvim.plugins = {
   -- Go
   "olexsmir/gopher.nvim",
   "leoluz/nvim-dap-go",
+
+  -- live_grep with ripgrep args
+  {
+    'nvim-telescope/telescope-live-grep-args.nvim',
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end
+  },
+
+  { "github/copilot.vim" },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      provider = "zendesk",
+      vendors = {
+        zendesk = {
+          api_key_name = "OPENAI_API_KEY",
+          endpoint = "https://ai-gateway.zende.sk/v1/",
+          model = "gpt-4o",
+          -- model = "claude-3.5-sonnet",
+          __inherited_from = "openai",
+        },
+      },
+      -- provider = "copilot",
+      -- endpoint = "https://ai-gateway.zende.sk/v1",
+      -- model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+
+      -- provider = "openai",
+      -- openai = {
+      --   endpoint = "https://ai-gateway.zende.sk/v1",
+      --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      --   temperature = 0,
+      --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      -- },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
 }
 
 vim.opt.relativenumber = true
+vim.opt.spell = true
 lvim.colorscheme = "gruvbox"
-
 
 -- Quickfix window Toggler
 -- https://www.reddit.com/r/neovim/comments/ol2vx4/how_to_toggle_quickfix_with_lua/
@@ -84,7 +167,7 @@ end
 
 -- Quickfix window
 lvim.builtin.which_key.mappings["z"] = { "<cmd>lua toggle_qf()<cr>", "Toggle Quickfix" }
-lvim.builtin.which_key.mappings["a"] = { "<cmd>lua toggle_qf_height()<cr>", "Toggle Quickfix Height" }
+lvim.builtin.which_key.mappings["x"] = { "<cmd>lua toggle_qf_height()<cr>", "Toggle Quickfix Height" }
 
 -- Disable horizontal trackpad scrolling
 lvim.keys.normal_mode["<ScrollWheelLeft>"] = "<nop>"
@@ -94,6 +177,17 @@ lvim.keys.insert_mode["<ScrollWheelRight>"] = "<nop>"
 
 -- save with ,,
 lvim.keys.normal_mode[",,"] = ":w<cr>"
+
+-- Delete and visual replace without replacing clipboard
+lvim.keys.visual_mode["p"] = '"+P'
+
+-- CoPilot, use C-J instead of <Tab> for accepting suggestions
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-G>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+
 
 ------------------------
 -- Telescope config
@@ -116,6 +210,12 @@ lvim.builtin.telescope.defaults.layout_config = {
   },
 }
 
+-- better find files
+lvim.builtin.which_key.mappings["f"] = {
+  "<cmd>Telescope find_files<cr>",
+  "Find files"
+}
+
 -- snippets with telescope
 lvim.builtin.which_key.mappings["s"]["s"] = {
   "<cmd>lua require('telescope').extensions.luasnip.luasnip{}<cr>",
@@ -128,10 +228,10 @@ lvim.builtin.which_key.mappings["s"]["g"] = {
   "Git files"
 }
 
--- better find files
-lvim.builtin.which_key.mappings["f"] = {
-  "<cmd>Telescope find_files<cr>",
-  "Find files"
+-- live_grep with ripgrep options
+lvim.builtin.which_key.mappings["s"]["a"] = {
+  "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>",
+  "Live grep with args"
 }
 
 -- file symbols
@@ -155,10 +255,13 @@ vim.api.nvim_create_user_command(
   { desc = 'Send given string to telescope live_grep', nargs = 1 }
 )
 
-
 -- Tab through buffers
 lvim.keys.normal_mode["<Tab>"] = ":bnext<CR>"
 lvim.keys.normal_mode["<S-Tab>"] = ":bprev<CR>"
+
+-- Organize imports via LSP
+-- lvim.lsp.buffer_mappings.normal_mode.go["1"] = "<cmd>lua vim.lsp.buf.organize_imports()<cr>"
+-- lvim.lsp.buffer_mappings.normal_mode.go["2"] = "organize imports"
 
 ------------------------
 -- Treesitter
@@ -183,12 +286,13 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "goimports", filetypes = { "go" } },
   { command = "gofumpt",   filetypes = { "go" } },
-  { name = "prettierd",    filetypes = { "javascript", "typescript", "typescriptreact" } }
+  { name = "prettierd",    filetypes = { "javascript", "typescript", "typescriptreact", "json" } }
 }
 
+-- Toggle with LvimToggleFormatOnSave
 lvim.format_on_save = {
   enabled = true,
-  pattern = "*.go",
+  pattern = { "*.lua", "*.go", "*.js", "*.ts" }
 }
 
 ------------------------
@@ -204,6 +308,36 @@ dapgo.setup()
 ------------------------
 -- LSP
 ------------------------
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+require('lspconfig').tsserver.setup({
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = 'relative',
+      importModuleSpecifierEnding = 'minimal',
+    },
+  },
+  commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports"
+    }
+  }
+})
+
+lvim.builtin.which_key.mappings["l"]["o"] = {
+  "<cmd>OrganizeImports<cr>",
+  "Organize Imports"
+}
+
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
 
 local lsp_manager = require "lvim.lsp.manager"
